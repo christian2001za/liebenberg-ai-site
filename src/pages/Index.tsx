aa172e707fecf2e8f302c0e5540169ce8554646e
christian2001za <christian2001za@gmail.com>
2025-11-06 12:19:48 +0100

    copy change to "Je ai receptionist..."

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -46,7 +46,7 @@ const Index = () => {
             </span>
           </h1>
           <p className="mb-6 text-lg text-muted-foreground md:text-xl lg:text-2xl">
-            Beantwoordt vragen, maakt afspraken en klinkt net als een echte medewerker.
+            Je AI receptionist beantwoordt vragen, maakt afspraken en klinkt net als een echte medewerker.
           </p>
           <div className="mt-19 mb-12 flex items-center justify-center gap-3">
             <Button
