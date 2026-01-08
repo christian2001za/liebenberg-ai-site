9f40149d2708565f1867a597e5aa419715ab5321
christian2001za <christian2001za@gmail.com>
2026-01-08 16:00:25 +0100

    change

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -263,7 +263,7 @@ const Index = () => {
                     AI Debatwijzer 2025
                   </h3>
                   <p className="text-muted-foreground mb-8 leading-relaxed">
-                     Een showcase van LLM en RAG technologie op grote schaal. We hebben een debat simulator ontwikkeld die informatie alleen haalt uit partijprogramma's inclusief citaten. Tijdens de verkiezingen in 2025 zijn er 300+ debatten gevoerd met onze software. 
+                     Een showcase van LLM en RAG technologie op grote schaal. We hebben een debat chatbot ontwikkeld die zijn informatie alleen haalt uit partijprogramma's, inclusief citaten. Tijdens de verkiezingen in 2025 zijn er 300+ debatten gevoerd met onze software. Dit soort toepassingen werken uitstekend voor interne chatbots om met uw eigen data te praten. 
                   </p>
                   
                   <div className="space-y-4 mb-8">
